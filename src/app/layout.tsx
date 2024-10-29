944349cb79d9f9e6cfdd0b71442740374b7d0d6e
Dani Portela <daniel.portela@cloudx.com>
2024-10-29 17:57:46 -0300

    Initial setup. Create CSS variables. Test API integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { Swords } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import keys from '../../public/keys.png'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ['latin'],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,8 +24,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn('py-6 px-5 antialiased bg-gradient-to-b from-pink-50 to-gray-0 to-20%', inter.className)}
       >
+        <Image
+          src={keys}
+          alt="keys"
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            width: "min(50%, 400px)",
+            zIndex: "-1"
+          }}
+        />
+
+        <header className="flex items-center">
+          <Swords />
+          <span
+            className="bg-gradient-to-r from-violet-900 to-violet-600 font-bold text-lg bg-clip-text text-transparent leading-normal"
+          >
+            Gaming Haven Z
+          </span>
+        </header>
         {children}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,82 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+			// Gray
+			'gray-1000': 'hsla(var(--gray-1000))',
+			'gray-500': 'hsla(var(--gray-500))',
+			'gray-200': 'hsla(var(--gray-200))',
+			'gray-0': 'hsla(var(--gray-0))',
+			// Pink
+			'pink-600': 'hsla(var(--pink-600))',
+			'pink-200': 'hsla(var(--pink-200))',
+			'pink-100': 'hsla(var(--pink-100))',
+			'pink-50': 'hsla(var(--pink-50))',
+			// Violet
+			'violet-900': 'hsla(var(--violet-900))',
+			'violet-600': 'hsla(var(--violet-600))',
+			'violet-100': 'hsla(var(--violet-100))',
+			'violet-50': 'hsla(var(--violet-50))',
+			// Red
+			'red-600': 'hsla(var(--red-600))',
+			// Green
+			'green-600': 'hsla(var(--green-600))',
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
