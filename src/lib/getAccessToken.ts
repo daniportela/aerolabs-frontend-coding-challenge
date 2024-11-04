5194f1e30a8231616748c0be6fd3c42a0c0fe038
Dani Portela <daniel.portela@cloudx.com>
2024-11-04 19:52:33 -0300

    Conver getAccessToken into a server action.

--- src/lib/getAccessToken.ts
+++ src/lib/getAccessToken.ts
@@ -1,12 +1,18 @@
+"use server"
+
 export default async function getAccessToken() {
-    const params = new URLSearchParams({
-        client_id: process.env.TWITCH_CLIENT_ID as string,
-        client_secret: process.env.TWITCH_CLIENT_SECRET as string,
-        grant_type: "client_credentials"
-    });
+    try {
+        const params = new URLSearchParams({
+            client_id: process.env.TWITCH_CLIENT_ID as string,
+            client_secret: process.env.TWITCH_CLIENT_SECRET as string,
+            grant_type: "client_credentials"
+        });
+    
+        const response = await fetch(`https://id.twitch.tv/oauth2/token?${params}`, { method: "POST" });
+        const tokenData = await response.json();
 
-    const response = await fetch(`https://id.twitch.tv/oauth2/token?${params}`, { method: "POST" });
-    const tokenData = await response.json();
-
-    return tokenData;
+        return tokenData;
+    } catch (error) {
+        throw new Error("Something went wrong when retrieving the access token:" + error)
+    }
 }
